a381eccc1322af2feb4b7267e3d81e75fe2d3621
fortyguide <stefano1994>
2022-04-28 11:04:46 +0200

    28/04/2022 Esempi mock per interfaccia List

---
+++ src/test/java/ListTest.java
@@ -0,0 +1,63 @@
+import org.junit.Test;
+
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ListTest {
+
+    @Test
+    public void testMockDelMetodoSizeDiList_DimensioneListaMaggioreDi0(){
+
+        List listMock = mock(List.class);
+        when(listMock.size()).thenReturn(2);
+
+        assertEquals(2,listMock.size());
+    }// Success
+
+    @Test
+    public void testMockDelMetodoSizeDiList_RitornaValoriMultipli(){
+
+        List listMock = mock(List.class);
+        when(listMock.size()).thenReturn(2).thenReturn(3);
+
+        assertEquals(2,listMock.size());
+        assertEquals(3,listMock.size());
+    }// Success
+
+    @Test
+    public void testMockDelMetodoGetDiList_DimensioneListaMaggioreDi0(){
+
+        List listMock = mock(List.class);
+        when(listMock.get(0)).thenReturn("Corso Mockito");
+
+        assertEquals("Corso Mockito",listMock.get(0));
+        assertEquals(null, listMock.get(1));
+    }// Success
+
+    @Test
+    public void testMockDelMetodoGetDiList_ArgumentMatcher(){
+
+        List listMock = mock(List.class);
+
+        /* Argument matcher*/
+        when(listMock.get(anyInt())).thenReturn("Corso Mockito");
+
+        assertEquals("Corso Mockito", listMock.get(0));
+        assertEquals("Corso Mockito", listMock.get(1));
+    }// Success
+
+    @Test(expected = RuntimeException.class)
+    public void testMockDelMetodoGetDiList_GeneroEccezione(){
+
+        List listMock = mock(List.class);
+
+        /* Argument matcher*/
+        when(listMock.get(anyInt())).thenThrow(new RuntimeException());
+
+        listMock.get(0);
+    }// Success
+}
