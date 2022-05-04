b145c7667ac6d9b3f09a99deba938c7ec73730c8
fortyguide <stefano1994>
2022-05-04 09:07:14 +0200

    04/05/2022 Ultimo commit

--- src/main/java/powermock/ClasseUtility.java
+++
@@ -1,9 +0,0 @@
-package powermock;
-
-public class ClasseUtility {
-    static int metodoStatico(long valore) {
-        // Qui si fa un po' di logica complessa...
-        throw new RuntimeException(
-                "Non voglio essere eseguito. Sarò comunque mockato.");
-    }
-}
